9838804aa708eb9d9ca3197b049cd446d5cd4d75
kalyan <kalyanKumarPokkula@gmail.com>
2024-02-21 12:45:10 +0530

    added small change for codepipeline to detect the changes that are made in to a repo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("CodeBuild Demo!");
 });
 
+app.get("/hello", (req, res) => {
+  res.send("Hello Wordl!");
+});
+
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
 });
